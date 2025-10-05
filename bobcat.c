8e358a4c1faa7fbc6286b54c3af8d788500065df
Jasdeep Matharu <jsmathar@ucsc.edu>
2025-10-04 17:45:38 -0700

    idk
    idk

--- bobcat.c
+++ bobcat.c
@@ -1,18 +1,19 @@
 #include <err.h>
 #include <fcntl.h>
 #include <stdbool.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
 #define BUFFER_SIZE 8192  // 8kb buffer for reading and writing
 
 int main(int argc, char* argv[]) {
   char buffer[BUFFER_SIZE];
   bool had_error = false;
+  ssize_t bytes_read;
   // if no arguments, read from standard input, as per instructions
   if (argc == 1) {
-    ssize_t bytes_read;
     while ((bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE)) > 0) {
       if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read) {
         warn("write failed");
@@ -28,8 +29,8 @@ int main(int argc, char* argv[]) {
   // if there are arguments, loop
   for (int i = 1; i < argc; i++) {
     char* current_arg = argv[i];
-    // our descriptor is the dash '-',and that
-    // indicates stadard in
+
+    // our descriptor is the dash '-',and that indicates stadard in
     int file_descriptor;
     if (strcmp(current_arg, "-") == 0) {
       file_descriptor = STDIN_FILENO;
@@ -41,9 +42,9 @@ int main(int argc, char* argv[]) {
         continue;
       }
     }
+
     // copy from file (or stdin) to stdout
     // same as above, but stdout
-    ssize_t bytes_read;
     while ((bytes_read = read(file_descriptor, buffer, BUFFER_SIZE)) > 0) {
       if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read) {
         warn("write failed");
@@ -55,8 +56,9 @@ int main(int argc, char* argv[]) {
       warn("read failed");
       had_error = true;
     }
-    // close the file if it was actually opened (not stdin) // and so no
-    // memory is leaked
+
+    // close the file if it was actually opened (not stdin)
+    // and so no memory is leaked
     if (file_descriptor != STDIN_FILENO) {
       if (close(file_descriptor) < 0) {
         warn("%s", current_arg);
@@ -64,5 +66,6 @@ int main(int argc, char* argv[]) {
       }
     }
   }
+
   return had_error ? 1 : 0;
 }
