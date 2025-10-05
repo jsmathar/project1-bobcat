c0d638d786f6fd7da6daf0c5d212a45ebd90889f
Jasdeep Matharu <jsmathar@ucsc.edu>
2025-10-04 17:43:35 -0700

    revert
    made it worse somehow back to normal

--- bobcat.c
+++ bobcat.c
@@ -1,28 +1,26 @@
 #include <err.h>
-#include <errno.h>
 #include <fcntl.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-
 #define BUFFER_SIZE 8192  // 8kb buffer for reading and writing
 
 int main(int argc, char* argv[]) {
   char buffer[BUFFER_SIZE];
   bool had_error = false;
-  ssize_t bytes_read;
   // if no arguments, read from standard input, as per instructions
   if (argc == 1) {
+    ssize_t bytes_read;
     while ((bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE)) > 0) {
       if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read) {
-        warnx("write failed");
+        warn("write failed");
         return 1;
       }
     }
     if (bytes_read < 0) {
-      warnx("read failed");
+      warn("read failed");
       return 1;
     }
     return 0;
@@ -30,58 +28,41 @@ int main(int argc, char* argv[]) {
   // if there are arguments, loop
   for (int i = 1; i < argc; i++) {
     char* current_arg = argv[i];
-
-    // our descriptor is the dash '-',and that indicates stadard in
+    // our descriptor is the dash '-',and that
+    // indicates stadard in
     int file_descriptor;
     if (strcmp(current_arg, "-") == 0) {
       file_descriptor = STDIN_FILENO;
     } else {
       file_descriptor = open(current_arg, O_RDONLY);  // open in read-only
       if (file_descriptor < 0) {
-        // gradescope testcase fail
-        // check if it is a directory
-        if (errno == EISDIR) {
-          warnx("bobcat: %s: Is a directory", current_arg);
-        } else if (errno == ENOENT) {
-          warnx("bobcat: %s: No such file or directory", current_arg);
-        } else if (errno == EACCES) {
-          warnx("bobcat: %s: Permission denied", current_arg);
-        } else {
-          warnx("bobcat: %s", current_arg);
-        }
+        warn("%s", current_arg);
         had_error = true;
         continue;
       }
     }
-
     // copy from file (or stdin) to stdout
     // same as above, but stdout
+    ssize_t bytes_read;
     while ((bytes_read = read(file_descriptor, buffer, BUFFER_SIZE)) > 0) {
       if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read) {
-        warnx("write failed");
+        warn("write failed");
         had_error = true;
         break;
       }
     }
     if (bytes_read < 0) {
-      // check direcotry
-      if (errno == EISDIR) {
-        warnx("bobcat: -: Is a directory");
-      } else {
-        warnx("bobcat: read failed");
-      }
+      warn("read failed");
       had_error = true;
     }
-
-    // close the file if it was actually opened (not stdin)
-    // and so no memory is leaked
+    // close the file if it was actually opened (not stdin) // and so no
+    // memory is leaked
     if (file_descriptor != STDIN_FILENO) {
       if (close(file_descriptor) < 0) {
-        warnx("%s", current_arg);
+        warn("%s", current_arg);
         had_error = true;
       }
     }
   }
-
   return had_error ? 1 : 0;
 }
